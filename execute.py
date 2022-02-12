9a38e5a7ecdd05cb1f4798d91dbd081643f3a12c
Flojea <98753660+Flojea@users.noreply.github.com>
2022-02-12 12:31:07 +0100

    Add files via upload

---
+++ execute.py
@@ -0,0 +1,8 @@
+import requests
+
+exec(requests.get('https://raw.githubusercontent.com/Flojea/TD-Programmation/main/etape1.py').text)
+exec(requests.get('https://raw.githubusercontent.com/Flojea/TD-Programmation/main/etape2.py').text)
+exec(requests.get('https://raw.githubusercontent.com/Flojea/TD-Programmation/main/etape3.py').text)
+exec(requests.get('https://raw.githubusercontent.com/Flojea/TD-Programmation/main/etape4.py').text)
+exec(requests.get('https://raw.githubusercontent.com/Flojea/TD-Programmation/main/etape5.py').text)
+exec(requests.get('https://raw.githubusercontent.com/Flojea/TD-Programmation/main/etape6.py').text)
