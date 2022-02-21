564dfb6158f1baa62ceb8c6eeb78283fc85414e6
Flojea <98753660+Flojea@users.noreply.github.com>
2022-02-21 14:39:46 +0100

    Update etape6.py

--- etape6.py
+++ etape6.py
@@ -5,25 +5,26 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 
-username = "athle.mois.pelletier@gmail.com"
-password = "Pelletouz"
-mail_from = "athle.mois.pelletier@gmail.com"
-mail_to = "athle.mois.pelletier@gmail.com"
+username = "mail.prog.atlhe@gmail.com"
+password = "passeword"
+mail_from = "mail.prog.atlhe@gmail.com"
+mail_to = "jeandel.florian@gmail.com"
 
-recipients = ['athle.mois.pelletier@gmail.com'] 
+recipients = ['jeandel.florian@gmail.com'] 
 emaillist = [elem.strip().split(',') for elem in recipients]
 msg = MIMEMultipart()
 msg['Subject'] = "Meilleures Performances en athlétisme sur les derniers 30 jours"
-msg['From'] = 'athle.mois.pelletier@gmail.com'
+msg['From'] = 'mail.prog.atlhe@gmail.com'
 
 html = """\
         <html>
           <head></head>
           <body>
             {0}
+            {1}
           </body>
         </html>
-""".format(df_mail.to_html(index=False))
+""".format(df_mail.to_html(),df_seuil.to_html())
 
 part1 = MIMEText(html, 'html')
 msg.attach(part1)
@@ -33,9 +34,11 @@ try:
 
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
-    server.login('athle.mois.pelletier@gmail.com',password)
+    server.login('mail.prog.atlhe@gmail.com',password)
     server.sendmail(msg['From'], emaillist , msg.as_string())
     server.close()
 
 except Exception as e:
     print("Error for connection: {}".format(e))
+
+
