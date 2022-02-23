7aafb8bbbb4294b7c1fb5e6dd0837cd2e03842ed
kyllianj <98753607+kyllianj@users.noreply.github.com>
2022-02-23 14:04:22 +0100

    Update execute.py

--- execute.py
+++ execute.py
@@ -1,5 +1,7 @@
 import requests
 
+give_mail = str(input('Ecrivez votre mail :'))
+
 exec(requests.get('https://raw.githubusercontent.com/Flojea/TD-Programmation/main/etape1.py').text)
 exec(requests.get('https://raw.githubusercontent.com/Flojea/TD-Programmation/main/etape2.py').text)
 exec(requests.get('https://raw.githubusercontent.com/Flojea/TD-Programmation/main/etape3.py').text)
