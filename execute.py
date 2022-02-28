058e3910c209a46aaac25f7545d245ac266901c6
Flojea <98753660+Flojea@users.noreply.github.com>
2022-02-28 16:14:53 +0100

    Update execute.py

--- execute.py
+++ execute.py
@@ -3,8 +3,8 @@ import requests
 give_mail = str(input('Ecrivez votre mail :'))
 
 exec(requests.get('https://raw.githubusercontent.com/Flojea/TD-Programmation/main/1 - Create calendar urls.py').text)
-exec(requests.get('https://raw.githubusercontent.com/Flojea/TD-Programmation/main/2 - Collect informations on each competition').text)
+exec(requests.get('https://raw.githubusercontent.com/Flojea/TD-Programmation/main/2 - Collect informations on each competition.py').text)
 exec(requests.get('https://raw.githubusercontent.com/Flojea/TD-Programmation/main/3 - Create results urls.py').text)
-exec(requests.get('https://raw.githubusercontent.com/Flojea/TD-Programmation/main/4 - Collect results for each competition.py.py').text)
+exec(requests.get('https://raw.githubusercontent.com/Flojea/TD-Programmation/main/4 - Collect results for each competition.py').text)
 exec(requests.get('https://raw.githubusercontent.com/Flojea/TD-Programmation/main/5 - Keep the best performances in each discipline.py').text)
 exec(requests.get('https://raw.githubusercontent.com/Flojea/TD-Programmation/main/6 - Send mail.py ').text)
