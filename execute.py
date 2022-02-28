ecf0f4702300bce24204488074aa98d1c4562f58
Flojea <98753660+Flojea@users.noreply.github.com>
2022-02-28 15:58:54 +0100

    Update execute.py

--- execute.py
+++ execute.py
@@ -2,9 +2,9 @@ import requests
 
 give_mail = str(input('Ecrivez votre mail :'))
 
-exec(requests.get('https://raw.githubusercontent.com/Flojea/TD-Programmation/main/etape1.py').text)
-exec(requests.get('https://raw.githubusercontent.com/Flojea/TD-Programmation/main/etape2.py').text)
-exec(requests.get('https://raw.githubusercontent.com/Flojea/TD-Programmation/main/etape3.py').text)
-exec(requests.get('https://raw.githubusercontent.com/Flojea/TD-Programmation/main/etape4.py').text)
-exec(requests.get('https://raw.githubusercontent.com/Flojea/TD-Programmation/main/etape5.py').text)
+exec(requests.get('https://raw.githubusercontent.com/Flojea/TD-Programmation/main/1 - Create calendar urls.py').text)
+exec(requests.get('https://raw.githubusercontent.com/Flojea/TD-Programmation/main/2 - Collect informations on each competition').text)
+exec(requests.get('https://raw.githubusercontent.com/Flojea/TD-Programmation/main/3 - Create results urls.py').text)
+exec(requests.get('https://raw.githubusercontent.com/Flojea/TD-Programmation/main/4 - Collect results for each competition.py.py').text)
+exec(requests.get('https://raw.githubusercontent.com/Flojea/TD-Programmation/main/5 - Keep the best performances in each discipline.py').text)
 exec(requests.get('https://raw.githubusercontent.com/Flojea/TD-Programmation/main/etape6.py').text)
