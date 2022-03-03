9fad62d5466f0fd5f153910afe683ee47ead685d
Flojea <98753660+Flojea@users.noreply.github.com>
2022-03-03 12:22:31 +0100

    Update 7 - Copy paste in python.py

--- 7 - Copy paste in python.py
+++ 7 - Copy paste in python.py
@@ -2,9 +2,9 @@ import requests
 
 give_mail = str(input('Ecrivez votre mail :'))
 
-exec(requests.get('https://raw.githubusercontent.com/Flojea/Athletics-Performances-Project/main/1 - Create calendar urls.py').text)
-exec(requests.get('https://raw.githubusercontent.com/Flojea/Athletics-Performances-Project/main/2 - Collect informations on each competition.py').text)
-exec(requests.get('https://raw.githubusercontent.com/Flojea/Athletics-Performances-Project/main/3 - Create results urls.py').text)
-exec(requests.get('https://raw.githubusercontent.com/Flojea/Athletics-Performances-Project/main/4 - Collect results for each competition.py').text)
-exec(requests.get('https://raw.githubusercontent.com/Flojea/Athletics-Performances-Project/main/5 - Keep the best performances in each discipline.py').text)
+exec(requests.get('https://raw.githubusercontent.com/Flojea/Athletics-Performances-Project/main/1%20-%20Create%20calendar%20urls.py').text)
+exec(requests.get('https://raw.githubusercontent.com/Flojea/Athletics-Performances-Project/main/2%20-%20Collect%20informations%20on%20each%20competition.py').text)
+exec(requests.get('https://raw.githubusercontent.com/Flojea/Athletics-Performances-Project/main/3%20-%20Create%20results%20urls.py').text)
+exec(requests.get('https://raw.githubusercontent.com/Flojea/Athletics-Performances-Project/main/4%20-%20Collect%20results%20for%20each%20competition.py').text)
+exec(requests.get('https://raw.githubusercontent.com/Flojea/Athletics-Performances-Project/main/5%20-%20Keep%20the%20best%20performances%20in%20each%20discipline.py').text)
 exec(requests.get('https://raw.githubusercontent.com/Flojea/Athletics-Performances-Project/main/6%20-%20Send%20mail.py').text)
